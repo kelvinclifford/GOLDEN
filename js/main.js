2a127409bc5993a673eee76c3b14621e29aa069a
Kelvin <kelvinclifford265@gmail.com>
2026-06-24 12:11:39 +0200

    modified index.html and deleted js/main.js

--- js/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  console.log('js/main.js loaded');
-
-  // Example: enable Bootstrap tooltips if present
-  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-  tooltipTriggerList.map(function (el) { return new bootstrap.Tooltip(el); });
-
-  // TODO: add custom initialization (carousel behavior, form handlers, etc.)
-});
